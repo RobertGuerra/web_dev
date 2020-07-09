9a073cfa4c6ffbf13893795112bac9c178e6f6d5
Rob <robert5guerra@gmail.com>
2020-07-09 08:07:54 -0700

    shows use of string delimiters

---
+++ php-demos/string-delimiters.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "</p>'Step outside, ' he said threateningly.</p>";
+
+echo '</p>"Step outside," he said threateningly.</p>';
+
+echo '"That\'s nice", he said';
+echo "<br>";
+echo "<br>";
+
+echo "Today's date is " . date('d F Y');
+
+?>
